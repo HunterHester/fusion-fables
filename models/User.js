a24cfccbe8c78c30dd50e92518c2cbb720d21f74
rmessett15 <rmessett15@gmail.com>
2023-05-12 16:00:09 -0500

    made changes and added model structure for user.js model, created relations in models/index.js, and created userData.json file for seeding the database

--- models/User.js
+++ models/User.js
@@ -3,46 +3,61 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
-    checkPassword(loginPw) {
+  checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
-    }
+  }
 }
 
-
 User.init(
-    {
-        id: {
-
-        },
-        username: {
-
-        },
-        password: {
-
-        },
-        email: {
-
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        hooks: {
-            beforeCreate: async (newBlogData) => {
-            newBlogData.password = await bcrypt.hash(newBlogData.password, 10);
-            newBlogData.email = await bcrypt.hash(newBlogData.email, 10);
-            return newBlogData;
-            },
-            beforeUpdate: async (updatedBlogData) => {
-            updatedBlogData.password = await bcrypt.hash(updatedBlogData.password, 10);
-            updatedBlogData.email = await bcrypt.hash(newBlogData.email, 10);
-            return updatedBlogDBlog
-            },
-        },
-        sequelize,
-        timestamps: true,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'user',
-        }
-    );
-    
-    module.exports = User;
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8-20],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newBlogData) => {
+        newBlogData.password = await bcrypt.hash(newBlogData.password, 10);
+        newBlogData.email = await bcrypt.hash(newBlogData.email, 10);
+        return newBlogData;
+      },
+      beforeUpdate: async (updatedBlogData) => {
+        updatedBlogData.password = await bcrypt.hash(
+          updatedBlogData.password,
+          10
+        );
+        updatedBlogData.email = await bcrypt.hash(newBlogData.email, 10);
+        return updatedBlogDBlog;
+      },
+    },
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
