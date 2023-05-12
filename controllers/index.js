ed85b753e58846cf1f499280361af45e6fecc712
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-12 18:07:12 -0500

    Write routes for api/controllers folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require('./');
 const homeRoutes = require('./homeRoutes');
 
 // blog and user routes
