f909b9cbce756d5d8758ebfe0a3d982f8628c148
eeast <eeasthope@gmail.com>
2023-05-13 12:35:17 -0500

    initial login functions, WIP

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,27 @@
 // create login form handler (POST)
-
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
 // create signup form handler (POST)
