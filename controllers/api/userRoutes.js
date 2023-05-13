a0c295f0047dcff14f3ae87ad16a11f49d4da185
eeast <eeasthope@gmail.com>
2023-05-13 16:20:09 -0500

    Working signup prototype, modified login.js, auth.js, userRoutes.js, and User.js. Changes in main and login hb files

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -14,6 +14,7 @@ router.post('/', async (req, res) => {
             res.status(200).json(userData);
         });
     } catch (err) {
+        console.log(err);
         res.status(400).json(err);
     }
 });
