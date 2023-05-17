e82fffdccb14550f0887b57dc6b1301d828b9cd1
Brian Hamlin <brianjhamlin@gmail.com>
2023-05-16 21:46:51 -0500

    add descriptive message to seed script, update seed file

--- seeds/postData.js
+++ seeds/postData.js
@@ -1,46 +1,16 @@
 const { Post } = require('../models');
 
 const postData = [
-    {
-        "title": "hello world",
-        "post_body": "when was the first time you surprised yourself by fixing something?",
-        "user_id": 1,
-        "is_public": true,
-        "allow_comments": true,
-        "numComments": 1
-    },
-    {
-        "title": "goodbye world",
-        "post_body": "that was sad",
-        "user_id": 4,
-        "is_public": true,
-        "allow_comments": false,
-        "numComments": 0
-    },
-    {
-        "title": "what are you doing",
-        "post_body": "my daughters yelling",
-        "user_id": 6,
-        "is_public": true,
-        "allow_comments": true,
-        "numComments": 5
-    },
-    {
-        "title": "asdlfkjasdf",
-        "post_body": "asdfasdfasdfsadfasdfasdf",
-        "user_id": 9,
-        "is_public": true,
-        "allow_comments": false,
-        "numComments": 0
-    },
-    {
-        "title": "pajksdfas",
-        "post_body": "wfghjfghjfghjfgjgfhj",
-        "user_id": 3,
-        "is_public": true,
-        "allow_comments": true,
-        "numComments": 1
-    },
+    {"title":"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.","post_body":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.","user_id":2,"is_public":true,"allow_comments":false},
+    {"title":"Etiam vel augue.","post_body":"Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.","user_id":2,"is_public":false,"allow_comments":false},
+    {"title":"Praesent blandit.","post_body":"Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.","user_id":2,"is_public":false,"allow_comments":false},
+    {"title":"Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.","post_body":"Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.","user_id":10,"is_public":false,"allow_comments":false},
+    {"title":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.","post_body":"Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.","user_id":10,"is_public":true,"allow_comments":true},
+    {"title":"Nulla justo.","post_body":"Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.","user_id":4,"is_public":true,"allow_comments":false},
+    {"title":"Curabitur at ipsum ac tellus semper interdum.","post_body":"Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.","user_id":7,"is_public":false,"allow_comments":true},
+    {"title":"Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.","post_body":"In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.","user_id":7,"is_public":false,"allow_comments":true},
+    {"title":"Curabitur at ipsum ac tellus semper interdum.","post_body":"Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.","user_id":10,"is_public":true,"allow_comments":false},
+    {"title":"Integer a nibh.","post_body":"Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.","user_id":3,"is_public":false,"allow_comments":true}
 ]
 
 const seedPosts = () => Post.bulkCreate(postData);
--- seeds/seed.js
+++ seeds/seed.js
@@ -7,8 +7,11 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedUsers();
+    console.log('---------- \n USERS seeded... \n ----------');
   await seedPosts();
+    console.log('---------- \n POSTS seeded... \n ----------');
   await seedComments();
+    console.log('---------- \n COMMENTS seeded... \n ----------');
 
   process.exit(0);
 };
