c6239b9e2fcb0ffae5021d29af0927db41c42435
rmessett15 <rmessett15@gmail.com>
2023-05-18 19:08:29 -0500

    added edit/delete button with proper placement and added js files with associated event listeners

--- public/js/editDeletePost.js
+++ public/js/editDeletePost.js
@@ -1,12 +1,22 @@
 const editDeletePostHandler = async (event) => {
     event.preventDefault();
-
-    const deleteBtn = document.querySelector('.delete-btn');
-    const editBtn = document.querySelector('.edit-btn');
-    if (deleteBtn || editBtn) {
-        console.log('Clicked me');
-    }
+    console.log('Clicked me');
 };
 
 document.querySelector('.delete-btn').addEventListener('click', editDeletePostHandler);
 document.querySelector('.edit-btn').addEventListener('click', editDeletePostHandler);
+
+const deleteButton = document.querySelectorAll('.delete-btn');
+const editButton = document.querySelectorAll('.edit-btn');
+
+for(let i=0; i<deleteButton.length; i++) {
+    deleteButton[i].addEventListener('click', editDeletePostHandler);
+}
+
+for (let i = 0; i < editButton.length; i++) {
+    editButton[i].addEventListener('click', editDeletePostHandler);
+}
+
+
+
+
