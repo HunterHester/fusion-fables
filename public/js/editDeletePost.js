1c1cacdf0e5ed26750ff82c10dcff033b0b745ce
rmessett15 <rmessett15@gmail.com>
2023-05-19 21:03:38 -0500

    added ability for user to have a comment on a post be inserted into the input box so they can update their comment -> still missing the ability to replace the id of the comment they initially were trying to edit -> right now it just adds a new comment without replacing the one the user wanted to edit

--- public/js/editDeletePost.js
+++ public/js/editDeletePost.js
@@ -1,7 +1,24 @@
 const editPostHandler = async (event) => {
     event.preventDefault();
     console.log('Clicked Me');
-    // const response = await fetch(`api/comment/${.id}`)
+    console.log(event.target);
+    console.log(document.querySelector('.card-text').textContent);
+    document.querySelector('#comment').value = document.querySelector('.card-text').textContent;
+    // const response = await fetch(`api/comment/${event.target.id}`, {
+    //     method: 'PUT',
+    //     body: JSON.stringify({
+    //         post_id,
+    //         comment_body,
+    //     }),
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    // });
+    // if (response.ok) {
+    //     document.location.reload();
+    // } else {
+    //     alert(response.statusText);
+    // }
 };
 
 const deletePostHandler = async (event) => {
@@ -26,8 +43,18 @@ for(let i=0; i<deleteButton.length; i++) {
 }
 
 for (let i = 0; i < editButton.length; i++) {
-    editButton[i].addEventListener('click', editPostHandler);
+    editButton[i].addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log('Clicked Me');
+        console.log(event.target);
+        console.log(this.id);
+        document.querySelector('#comment').value = document.getElementById('' + this.id).parentElement.previousElementSibling.textContent;
+        console.log(document.getElementById('' + this.id).parentElement.previousElementSibling.textContent);
+    });
 }
+
+// Need variable for placeholder which becomes the id of the comment i click on
+// When i click submit I will run the call, then i will update the id within the call to the previously created variable (use that id for the put call)
 
 
 
