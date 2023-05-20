c77ffa6c56581f770016b6eec252ddc55a70c590
eeast <eeasthope@gmail.com>
2023-05-20 14:58:26 -0500

    working filtering on edit and delete buttons. Now only shows up on your blog page if you are logged in.

--- public/js/cardClick.js
+++ public/js/cardClick.js
@@ -1,8 +1,6 @@
 const goToPost = async (event) => {
     event.preventDefault();
     event.stopPropagation();
-
-    console.log('Clicked');
     
     window.location.replace(`/u/${event.currentTarget.dataset.user}/${event.currentTarget.dataset.id}`);
 };
@@ -12,5 +10,3 @@ const postLinks = document.querySelectorAll('#post-link');
 for(let i = 0; i < postLinks.length; i++) {
     postLinks[i].addEventListener('click', goToPost);
 };
-
-console.log('script loaded');
